b42009e6c419293153258e10d3e8121e1d4d528a
danmatuoka <dan_mtk@hotmail.com>
2023-03-18 10:58:24 -0300

    feat: hash password

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -1,13 +1,15 @@
 const User = require("./../models/User");
+const bcrypt = require('bcrypt');
 
 exports.signUp = async (req, res, next) => {
   try {
-    // Criar a criptografia da senha antes de passar para o banco de dados
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
     const response = await User.create({
       name: req.body.name,
       birthday: req.body.birthday,
       email: req.body.birthday,
-      password: req.body.password
+      password: hashedPassword
     })
 
     res.json({ id: response.id })
