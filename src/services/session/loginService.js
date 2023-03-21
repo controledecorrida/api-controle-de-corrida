1daea2145431f72480237d32b8b8e1ce4816e533
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-21 13:21:06 -0300

    feat: implementa validação no service de login

--- src/services/session/loginService.js
+++ src/services/session/loginService.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
 const crypto = require('./../crypto');
+const { userSchema } = require("../../schemas/user");
+const { validateBody } = require("../../utils/validateBody");
 
 console.log(process.env.SECRET_KEY)
 
-const loginService = async ({email, password}) => {
+const loginService = async ({ email, password }) => {
+  validateBody(userSchema, { email, password });
+
   const user = await User.findOne({ where: { email: email } });
 
   if (!user) {
